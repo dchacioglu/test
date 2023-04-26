b35e4a4d63bd225574ba83f36d30ac8fcfd7bc24
dchacioglu <dchacioglu@gmail.com>
2023-04-27 00:06:05 +0300

    lest eklendi

---
+++ lets.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int t;
+    scanf("%d", &t);
+    int i = 0;
+    
+    while(i<t)
+    {
+            char x[100];
+            scanf(" %[^\n]s", x);
+            //getchar();
+            int j = 0;
+            int len = strlen(x);
+            while(j < len)
+            {
+                printf("%c", x[j]);
+            j += 2;
+            }
+            
+            printf(" ");
+            
+            j = 1;
+            while(j < len)
+            {
+                printf("%c", x[j]);
+            j += 2;
+            }
+            
+    printf("\n");
+    i++;
+    }
+    return 0;
+}
